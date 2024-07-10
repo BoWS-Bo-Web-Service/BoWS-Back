55a1d69410d9b7e7fe26762c2b8a8f47edb42a0c
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-07-10 14:26:47 +0900

    initial commit

---
+++ src/main/java/codesquad/bows/BowsApplication.java
@@ -0,0 +1,13 @@
+package codesquad.bows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowsApplication.class, args);
+	}
+
+}
