37d0ae3da2ac6ef45aebdea2559c4e0755b76ad3
imojaek <imojaek@gmail.com>
2024-08-01 15:01:02 +0900

    [feat/#10] feat: 회원가입 시 Role_User 부여

--- src/main/java/codesquad/bows/member/service/MemberService.java
+++ src/main/java/codesquad/bows/member/service/MemberService.java
@@ -3,10 +3,15 @@ package codesquad.bows.member.service;
 
 import codesquad.bows.member.dto.MemberRegisterData;
 import codesquad.bows.member.entity.Member;
+import codesquad.bows.member.entity.Role;
+import codesquad.bows.member.entity.RoleName;
 import codesquad.bows.member.repository.MemberRepository;
+import codesquad.bows.member.repository.RoleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -14,16 +19,22 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    private final RoleRepository roleRepository;
+
     private final PasswordEncoder passwordEncoder;
 
     public void register(MemberRegisterData data) {
         if (memberRepository.findByUsername(data.getUsername()).isPresent()) {
             throw new IllegalArgumentException("이미 존재하는 아이디입니다.");
         }
+
+        Role userRole = roleRepository.findByName(RoleName.USER.name())
+                .orElseThrow(() -> new IllegalArgumentException("해당하는 Role이 존재하지 않습니다."));
+
         Member member = Member.builder()
                 .username(data.getUsername())
                 .password(passwordEncoder.encode(data.getPassword()))
-                // todo : role 줘야함
+                .roles(Set.of(userRole))
                 .build();
         memberRepository.save(member);
     }
