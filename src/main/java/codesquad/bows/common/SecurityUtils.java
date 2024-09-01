f13862d5857521ff39ab9547f4e1b5d0b0346ef7
imojaek <imojaek@gmail.com>
2024-09-01 17:19:26 +0900

    [fix/#51] feat: 역할군별 권한 이름 및 세부사항 변경

--- src/main/java/codesquad/bows/common/SecurityUtils.java
+++ src/main/java/codesquad/bows/common/SecurityUtils.java
@@ -3,7 +3,10 @@ package codesquad.bows.common;
 import codesquad.bows.global.security.user.CustomUserDetails;
 import codesquad.bows.member.exception.UnauthenticatedException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Collection;
 
 public class SecurityUtils {
     public static String getLoginUserId() {
@@ -13,5 +16,11 @@ public class SecurityUtils {
             return userDetails.getUsername();
         }
         throw new UnauthenticatedException();
+    }
+
+    public static boolean hasAuthority(String targetAuthority) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getAuthorities().stream()
+                .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals(targetAuthority));
     }
 }
--- src/main/java/codesquad/bows/member/entity/AuthorityName.java
+++ src/main/java/codesquad/bows/member/entity/AuthorityName.java
@@ -2,6 +2,9 @@ package codesquad.bows.member.entity;
 
 public enum AuthorityName {
     ADMIN,
-    PROJECT_READ,
-    PROJECT_EDIT
+    PROJECT_CREATE,
+    PROJECT_READ_ALL,
+    PROJECT_READ_OWN,
+    PROJECT_EDIT_OWN,
+    PROJECT_EDIT_ALL
 }
